6fbe88c034c47adb8b155ddb809d09ff5eed8fb6
ntinhhcm <ntinh.hcm@gmail.com>
2019-02-28 21:28:44 +0700

    Build source

---
+++ precache-manifest.477c371dd6c51ffa8fc5767b89300c4b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/vue-ua/robots.txt"
+  },
+  {
+    "revision": "b37f28832b3ef16b79a1",
+    "url": "/vue-ua/js/chunk-vendors.818aae16.js"
+  },
+  {
+    "revision": "97a84d205b0d81f3ee4d",
+    "url": "/vue-ua/js/app.4f20e466.js"
+  },
+  {
+    "revision": "0305328c28d7bd1b1639",
+    "url": "/vue-ua/js/about.799d9376.js"
+  },
+  {
+    "revision": "792dc0c7f0a65b66a88e07038e9d2153",
+    "url": "/vue-ua/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/vue-ua/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "97a84d205b0d81f3ee4d",
+    "url": "/vue-ua/css/app.bdcfe4f4.css"
+  }
+];
